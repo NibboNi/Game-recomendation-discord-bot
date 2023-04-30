d20bdd1345167bf9847445ae5519e4041a16429a
NibboNi <131214845+NibboNi@users.noreply.github.com>
2023-04-29 23:22:28 -0600

    Actualizado bot.dart
    + Se agrego la base de la funcionalidad de recomendaciones

--- lib/discord_bot.dart
+++ lib/discord_bot.dart
@@ -2,17 +2,24 @@ import 'package:dotenv/dotenv.dart';
 import 'package:nyxx/nyxx.dart';
 
 void bot(){
+  List<String> juegos = [
+    "Juego 1",
+    "Juego 2",
+    "Juego 3",
+    "Juego 4",
+    "Juego 5",
+    "Juego 6",
+    ];
 
   final dotenv = DotEnv(includePlatformEnvironment: true)..load();
 
   final bot = NyxxFactory.createNyxxWebsocket(dotenv['TOKEN']!, GatewayIntents.allUnprivileged | GatewayIntents.messageContent);
 
   bot.eventsWs.onMessageReceived.listen((event) {
-    if (event.message.content.toLowerCase() == 'hola') {
+    if (event.message.content.toLowerCase() == '!recomienda') {
     final replyBuilder = ReplyBuilder.fromMessage(event.message);
     final allowedMentions = AllowedMentions()..allow(reply: false);
-
-    event.message.channel.sendMessage(MessageBuilder.content('adios')..replyBuilder = replyBuilder..allowedMentions = allowedMentions);
+    event.message.channel.sendMessage(MessageBuilder.content("Te recomiendo ${(juegos..shuffle()).first}")..replyBuilder = replyBuilder..allowedMentions = allowedMentions);
     }
   });
 
